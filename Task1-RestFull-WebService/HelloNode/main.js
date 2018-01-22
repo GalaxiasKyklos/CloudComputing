0ce9d4c505e97a320263cb84c833df80995135be
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-01-22 10:58:58 -0600

    Fase 0

---
+++ Task1-RestFull-WebService/HelloNode/main.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    ContentType: 'text/plain'
+  })
+  res.end('Hello World\n')
+})
+
+server.listen(8000)
+
+console.log('Server running at http://localhost:8000/')
