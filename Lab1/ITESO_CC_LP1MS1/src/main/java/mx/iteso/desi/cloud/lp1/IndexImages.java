db510904798a7c78f60ccad265e1b4b0047338e0
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-02-18 20:23:38 -0600

    Implement Indexer
    asd
    
    Fix IndexImage

--- Lab1/ITESO_CC_LP1MS1/src/main/java/mx/iteso/desi/cloud/lp1/IndexImages.java
+++ Lab1/ITESO_CC_LP1MS1/src/main/java/mx/iteso/desi/cloud/lp1/IndexImages.java
@@ -1,7 +1,7 @@
 package mx.iteso.desi.cloud.lp1;
 
 import java.io.IOException;
-import java.util.HashSet;
+
 import mx.iteso.desi.cloud.keyvalue.IKeyValueStorage;
 import mx.iteso.desi.cloud.keyvalue.KeyValueStoreFactory;
 import mx.iteso.desi.cloud.keyvalue.ParseTriples;
@@ -11,42 +11,71 @@ import mx.iteso.desi.cloud.keyvalue.Triple;
 public class IndexImages {
   ParseTriples parser;
   IKeyValueStorage imageStore, titleStore;
-    
+
   public IndexImages(IKeyValueStorage imageStore, IKeyValueStorage titleStore) {
-	  this.imageStore = imageStore;
-	  this.titleStore = titleStore;
+    this.imageStore = imageStore;
+    this.titleStore = titleStore;
   }
-      
-  public void run(String imageFileName, String titleFileName) throws IOException
-  {
-    // TODO: This method should load all images and titles 
-    //       into the two key-value stores.
+
+  public void run(String imageFileName, String titleFileName) throws IOException {
+    ParseTriples parseImages = new ParseTriples("/Users/saul/Downloads/" + imageFileName);
+    Triple triple;
+    while ((triple = parseImages.getNextTriple()) != null) {
+      String predicade = triple.getPredicate();
+      if (predicade.equals("http://xmlns.com/foaf/0.1/depiction")) {
+        String key = triple.getSubject();
+        String value = triple.getObject();
+        this.imageStore.addToSet(key, value);
+      }
+    }
+    parseImages.close();
+
+    ParseTriples parseTitles = new ParseTriples("/Users/saul/Downloads/" + titleFileName);
+    while ((triple = parseTitles.getNextTriple()) != null) {
+      String predicade = triple.getPredicate();
+      String value = triple.getSubject();
+      if (this.imageStore.exists(value)) {
+        if (predicade.equals("http://www.w3.org/2000/01/rdf-schema#label")) {
+          String key = triple.getObject();
+          key = key.replace("[^A-Za-z ]", "");
+          // CamelCase-spliting fuckery:
+          // https://stackoverflow.com/questions/7593969/regex-to-split-camelcase-or-titlecase-advanced
+          String[] keys = key.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
+          for (String keyWord : keys) {
+            keyWord = keyWord.trim();
+            for (String finalKey : keyWord.split(" ")) {
+              finalKey = PorterStemmer.stem(finalKey.toLowerCase());
+              if (!finalKey.equals("Invalid term") && !finalKey.equals("No term entered")) {
+                this.titleStore.addToSet(finalKey, value);
+              }
+            }
+          }
+        }
+      }
+    }
+    parseTitles.close();
   }
-  
+
   public void close() {
-	  //TODO: close the databases;
+    this.imageStore.close();
+    this.titleStore.close();
   }
-  
-  public static void main(String args[])
-  {
-    // TODO: Add your own name here
-    System.out.println("*** Alumno: _____________________ (Exp: _________ )");
+
+  public static void main(String args[]) {
+    System.out.println("*** Alumno: Saúl Ponce (Exp: is699399)");
     try {
 
-      IKeyValueStorage imageStore = KeyValueStoreFactory.getNewKeyValueStore(Config.storeType, 
-    			"images");
-      IKeyValueStorage titleStore = KeyValueStoreFactory.getNewKeyValueStore(Config.storeType, 
-  			"terms");
-
+      IKeyValueStorage imageStore = KeyValueStoreFactory.getNewKeyValueStore(Config.storeType, "images");
+      IKeyValueStorage titleStore = KeyValueStoreFactory.getNewKeyValueStore(Config.storeType, "terms");
 
       IndexImages indexer = new IndexImages(imageStore, titleStore);
       indexer.run(Config.imageFileName, Config.titleFileName);
+      indexer.close();
       System.out.println("Indexing completed");
-      
+
     } catch (Exception e) {
       e.printStackTrace();
       System.err.println("Failed to complete the indexing pass -- exiting");
     }
   }
 }
-
