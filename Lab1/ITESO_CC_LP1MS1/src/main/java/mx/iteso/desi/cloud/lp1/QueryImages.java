65247cb5541ed46547964df8f455ae9405e3ad75
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-02-18 20:24:01 -0600

    Implement Querier

--- Lab1/ITESO_CC_LP1MS1/src/main/java/mx/iteso/desi/cloud/lp1/QueryImages.java
+++ Lab1/ITESO_CC_LP1MS1/src/main/java/mx/iteso/desi/cloud/lp1/QueryImages.java
@@ -4,50 +4,52 @@ import java.util.Set;
 import java.util.Iterator;
 import java.util.HashSet;
 import mx.iteso.desi.cloud.keyvalue.KeyValueStoreFactory;
+import mx.iteso.desi.cloud.keyvalue.PorterStemmer;
 import mx.iteso.desi.cloud.keyvalue.IKeyValueStorage;
 
 public class QueryImages {
   IKeyValueStorage imageStore;
   IKeyValueStorage titleStore;
-	
-  public QueryImages(IKeyValueStorage imageStore, IKeyValueStorage titleStore) 
-  {
-	  this.imageStore = imageStore;
-	  this.titleStore = titleStore;
-  }
-	
-  public Set<String> query(String word)
-  {
-    // TODO: Return the set of URLs that match the given word,
-    //       or an empty set if there are no matches
-    return new HashSet<String>();
-  }
-        
-  public void close()
-  {
-    // TODO: Close the databases
-  }
-	
-  public static void main(String args[]) 
-  {
-    // TODO: Add your own name here
-    System.out.println("*** Alumno: _____________________ (Exp: _________ )");
-    
-    // TODO: get KeyValueStores
-    IKeyValueStorage imageStore = null;
-    IKeyValueStorage titleStore = null;
-    
-    QueryImages myQuery = new QueryImages(imageStore, titleStore);
 
-    for (int i=0; i<args.length; i++) {
-      System.out.println(args[i]+":");
-      Set<String> result = myQuery.query(args[i]);
-      Iterator<String> iter = result.iterator();
-      while (iter.hasNext()) 
-        System.out.println("  - "+iter.next());
+  public QueryImages(IKeyValueStorage imageStore, IKeyValueStorage titleStore) {
+    this.imageStore = imageStore;
+    this.titleStore = titleStore;
+  }
+
+  public Set<String> query(String word) {
+    Set<String> values = titleStore.get(PorterStemmer.stem(word.toLowerCase()));
+    Set<String> urls = new HashSet<>();
+    for (String v : values) {
+      urls.addAll(imageStore.get(v));
     }
-    
-    myQuery.close();
+    return urls;
+  }
+
+  public void close() {
+    this.imageStore.close();
+    this.titleStore.close();
+  }
+
+  public static void main(String args[]) {
+    System.out.println("*** Alumno: Saúl Ponce (Exp: is699399)");
+
+    try {
+      IKeyValueStorage imageStore = KeyValueStoreFactory.getNewKeyValueStore(Config.storeType, "images");
+      IKeyValueStorage titleStore = KeyValueStoreFactory.getNewKeyValueStore(Config.storeType, "terms");
+
+      QueryImages myQuery = new QueryImages(imageStore, titleStore);
+
+      for (int i = 0; i < args.length; i++) {
+        System.out.println(args[i] + ":");
+        Set<String> result = myQuery.query(args[i]);
+        Iterator<String> iter = result.iterator();
+        while (iter.hasNext())
+          System.out.println("  - " + iter.next());
+      }
+
+      myQuery.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
-
