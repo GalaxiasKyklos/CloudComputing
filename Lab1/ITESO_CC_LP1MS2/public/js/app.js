1ea85123fdfe636bd84c6eb12479ddfd737c9c6b
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-02-24 13:06:24 -0600

    Add search front end

--- Lab1/ITESO_CC_LP1MS2/public/js/app.js
+++ Lab1/ITESO_CC_LP1MS2/public/js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $("#version").html("v0.14");
+  let results = [];
+  let currentPage = 0;
+  let total = 0;
   
   $("#searchbutton").click( function (e) {
     displayModal();
@@ -12,23 +15,34 @@ $(document).ready(function() {
   });
   
   function displayModal() {
-    $(  "#myModal").modal('show');
+    $("#myModal").modal('show');
 
     $("#status").html("Searching...");
     $("#dialogtitle").html("Search for: "+$("#searchfield").val());
     $("#previous").hide();
     $("#next").hide();
     $.getJSON('/search/' + $("#searchfield").val() , function(data) {
+      results = [];
       renderQueryResults(data);
     });
   }
   
   $("#next").click( function(e) {
-
+    $('#previous').show();
+    updatePage(++currentPage);
+    if (currentPage * 4 >= total - 4) {
+      $("#next").hide();
+    }
   });
   
   $("#previous").click( function(e) {
-  
+    updatePage(--currentPage);
+    if (currentPage * 4 < total) {
+      $("#next").show();
+    }
+    if (currentPage === 0) {
+      $('#previous').hide();
+    }
   });
 
   function renderQueryResults(data) {
@@ -37,10 +51,24 @@ $(document).ready(function() {
       $("#status").html("Error: "+data.error);
     } else {
       $("#status").html(""+data.num_results+" result(s)");
-
-      $("#next").show();
-      $("#previous").show();
-      
+      data.results.map(r => {
+          results.push(`<img src="${r}" width="100" heigh="auto">`);
+      });
+      updatePage(currentPage);
+      total = results.length;
+      if (total > 4) {
+        $("#next").show();
+      }
      }
+   }
+
+   function updatePage(page) {
+    $("td[id^='photo']").each((el, i) => {
+      $(i).empty();
+    });
+    results.slice(page * 4, page * 4 + 4).map((r, i) => {
+      $('#photo' + i).empty();
+      $('#photo' + i).append(r);
+    });
    }
 });
