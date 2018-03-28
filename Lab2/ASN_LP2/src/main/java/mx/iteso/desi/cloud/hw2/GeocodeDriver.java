177f7a07f0b0fb9e11ad136660b12d69c4a2c889
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-03-28 13:02:41 -0600

    Working thing

--- Lab2/ASN_LP2/src/main/java/mx/iteso/desi/cloud/hw2/GeocodeDriver.java
+++ Lab2/ASN_LP2/src/main/java/mx/iteso/desi/cloud/hw2/GeocodeDriver.java
@@ -5,13 +5,12 @@ import mx.iteso.desi.cloud.Triple;
 public class GeocodeDriver {
 
   public static void main(String[] args) throws Exception {
-  
     if (args.length != 2) {
       System.err.println("Usage: GeocodeDriver <input path> <output path>");
       System.exit(-1);
     }
-    
+
     /* TODO: Your driver code here */
-	
+
   }
 }
